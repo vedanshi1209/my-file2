b4e1d7dffa6a04f086d6fcaac3571fdab068f8e0
vedanshi1209 <vedanshipatidar0@gmail.com>
2025-07-01 18:29:33 +0530

    first commit

---
+++ 01_list01.py
@@ -0,0 +1,9 @@
+a=[12,14,11,16,18] #is called list
+for i in a:
+    a==[i]
+    print(i)
+a=["ram", "shayam", "ramesh", "suresh", "ramu" ]
+for i in a:
+    a==[i]
+    print(i)
+# this is valid for string as well as integer
